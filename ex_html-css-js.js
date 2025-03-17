8ec17951ca76974d79cc2ff9ca4ecd45c9b60b58
Emilio1701 <efermar1701@g.educaand.es>
2025-03-17 18:38:04 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,2 +1,22 @@
 document.getElementById("boton").onclick = function(){
     document.body.classList.toggle("oscuro")
+}
+let envio =document.getElementById("envio")
+envio.addEventListener("mouseover", function(){
+    envio.style.color = "red"
+})
+envio.addEventListener("mouseout", function(){
+    envio.style.color = "black"
+})
+document.getElementById("Fotrmulario").oninput=function(){
+    let obligatorio = document.getElementById("obligatorio").value.trim()
+    let mensaje = document.getElementById("mensaje")
+    if(obligatorio === ""){
+        event.preventDefault()
+        mensaje.textContent= "Este campo es obligatorio"
+        mensaje.style.color= "red"
+    }
+}
+if (!Email.includes("@")){
+    console.log("El correo debe contener @")
+}
