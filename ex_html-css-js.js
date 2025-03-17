ea136b0f0a611956fd277ea412f47318d2c2a233
Emilio1701 <efermar1701@g.educaand.es>
2025-03-17 18:59:23 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,22 +1,13 @@
 document.getElementById("oscuro").onclick = function(){
     document.body.classList.toggle("oscuro")
 }
-let envio =document.getElementById("envio")
-envio.addEventListener("mouseover", function(){
-    envio.style.color = "red"
+let boton =document.getElementById("boton")
+boton.addEventListener("mouseover", function(){
+    boton.style.color = "red"
 })
-envio.addEventListener("mouseout", function(){
-    envio.style.color = "black"
+boton.addEventListener("mouseout", function(){
+    boton.style.color = "black"
 })
-document.getElementById("Fotrmulario").oninput=function(){
-    let obligatorio = document.getElementById("obligatorio").value.trim()
-    let mensaje = document.getElementById("mensaje")
-    if(obligatorio === ""){
-        event.preventDefault()
-        mensaje.textContent= "Este campo es obligatorio"
-        mensaje.style.color= "red"
-    }
-}
 if (!Email.includes("@")){
     console.log("El correo debe contener @")
 }
